d5d480e6ad0c69bf0d67f87134bb4552a19399f4
Murat Kudainetov <murat.kudainetov@gmail.com>
2016-05-17 15:28:23 +0300

    fixes after 1st attempt complete with minimize
    fixes after 1st attempt complete with minimize

--- js/script.js
+++ js/script.js
@@ -2,29 +2,22 @@ var link = document.querySelector(".feedback-btn");
 var popup = document.querySelector(".modal-feedback");
 var overlay = document.querySelector(".modal-overlay");
 var close = popup.querySelector(".modal-feedback-close");
-
-      var storage = localStorage.getItem("login");
-
-      link.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.add("modal-feedback-show");
-        overlay.classList.add("modal-overlay-show");
-
-        
-
-      });
-      close.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.remove("modal-feedback-show");
-        overlay.classList.remove("modal-overlay-show");
-      });
-
-
+var storage = localStorage.getItem("login");
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-feedback-show");
+    overlay.classList.add("modal-overlay-show");
+  });
+  close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-feedback-show");
+    overlay.classList.remove("modal-overlay-show");
+  });
  window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (popup.classList.contains("modal-feedback-show")) {
-            popup.classList.remove("modal-feedback-show");
-            overlay.classList.remove("modal-overlay-show");
-          }
-        }
-      });
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-feedback-show")) {
+      popup.classList.remove("modal-feedback-show");
+      overlay.classList.remove("modal-overlay-show");
+    }
+  }
+});
